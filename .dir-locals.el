0f8b027089cf96403982dc8ad356f9ecbf0d6ec2
Mathias Rüdiger <ruediger@blueboot.org>
2025-05-27 10:45:27 +0200

    switch to nixos-hardware based setup
    https://github.com/nix-community/raspberry-pi-nix has been archived.

--- .dir-locals.el
+++
@@ -1,1 +0,0 @@
-((nix-mode . ((compile-command . "nixos-rebuild switch --flake '.#pi5-a' --use-remote-sudo --build-host pi5-a.local --target-host pi5-a.local"))))
--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
   description = "Raspberry Pi 5";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    raspberry-pi-nix.url = "github:nix-community/raspberry-pi-nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   nixConfig = {
@@ -18,28 +22,12 @@
     inputs:
     with inputs;
     {
-      nixosConfigurations =
-        let
-          configure = hostname: roles: nixpkgs.lib.nixosSystem {
-            specialArgs = { inherit hostname roles inputs; };
-            system = "aarch64-linux";
-            modules = [
-              raspberry-pi-nix.nixosModules.raspberry-pi
-              raspberry-pi-nix.nixosModules.sd-image
-              ./modules/base-config.nix
-              ./modules/networking.nix
-              ./modules/users.nix
-              ./modules/openssh.nix
-              ./modules/nix.nix
-              ./modules/headless.nix
-              ./modules/prometheus.nix
-              ./modules/grafana.nix
-              ./modules/kubernetes.nix
-            ];
-          };
-          in {
-            pi5-a = configure "pi5-a" [ "node" "master" ];
-            pi5-b = configure "pi5-b" [ "node" ];
-          };
+      nixosConfigurations.pi5-test = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          nixos-generators.nixosModules.all-formats
+          nixos-hardware.nixosModules.raspberry-pi-5
+        ];
+      };
     };
 }
