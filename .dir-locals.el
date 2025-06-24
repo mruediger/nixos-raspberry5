3fa52e6bb119a86c8140887289cde0c880b4a3b3
Mathias Rüdiger <ruediger@blueboot.org>
2025-06-24 13:24:57 +0200

    add .dir-locals for compilation

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nix-mode . ((compile-command . "nixos-rebuild switch --flake '.#pi5-test'"))))
