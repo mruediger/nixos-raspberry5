862cc3d3d9413e75bd988bf14e32e9779b722910
Mathias Rüdiger <ruediger@blueboot.org>
2025-06-24 14:10:13 +0200

    update dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nix-mode . ((compile-command . "nixos-rebuild switch --flake '.#pi5-test'"))))
+((nix-mode . ((compile-command . "nix build '.#nixosConfigurations.pi5-test.config.formats.sd-aarch64'"))))
